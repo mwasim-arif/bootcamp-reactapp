779456893f0de98514198dacd6053ae55c151ff5
Muhammad Wasim Arif <76242160+mwasim-arif@users.noreply.github.com>
2020-12-24 22:23:56 +0500

    Update Data loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
+import Alpha from './alpha.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hellow World</h1>
+      <Alpha empireName='British Empire' />
+      <hr />
+      <Alpha empireName='Muongol Empire' />
+      <hr />
+      <Alpha empireName='Russian Empire' />
+      <hr />
+      <Alpha empireName='Spanish Empire' />
     </div>
   );
 }
---
+++ src/alpha.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Alpha (props) {
+    return(
+        <div>
+            <h1>In the history {props.empireName} has significan area.</h1>
+            <h1>When {props.empireName} started to make wrong decisions and not able to maintain justice, they lost hold.</h1>
+
+        </div>
+    )
+}
+
+export default Alpha;
